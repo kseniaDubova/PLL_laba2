767495a7c91ee6246345c41267cb31843f92d084
kseniaDubova <113327030+kseniaDubova@users.noreply.github.com>
2024-03-29 22:33:31 +0400

    openMP

--- PLL_laba2/main.cpp
+++ PLL_laba2/main.cpp
@@ -1,118 +1,99 @@
-
 #include <fstream>
 #include <vector>
 #include <chrono>
-#include <iomanip>
 #include <iostream>
 #include <stdio.h>
-#include <string>
 #include <stdlib.h>
+#include <omp.h>
+#include <string>
 using namespace std;
 
-
-void create_file_matrix(string file_name, const vector<vector<int>>& matrix)
-{
-    ofstream out;
-    out.open(file_name);
-    if (out.is_open())
-    {
-        for (int i = 0; i < matrix.size(); i++)
-        {
-            for (int j = 0; j < matrix.size(); j++)
-            {
-                out << " " << matrix[i][j];
-            }
-            out << endl;
+void create_file_matrix(string file_name, const vector<vector<int>>& matrix) {
+    ofstream out(file_name);
+    for (const auto& row : matrix) {
+        for (int val : row) {
+            out << " " << val;
         }
+        out << endl;
     }
-    out.close();
 }
 
-void create_matrix(string file_name, int size)
-{
+void create_matrix(string file_name, int size) {
     vector<vector<int>> m(size, vector<int>(size));
-
-    for (int i = 0; i < m.size(); i++)
-    {
-        for (int j = 0; j < m.size(); j++)
-        {
-            m[i][j] = rand() % 500;
+    for (auto& row : m) {
+        for (int& val : row) {
+            val = rand() % 500;
         }
     }
     create_file_matrix(file_name, m);
 }
-void read_matrix(vector<vector<int>>& matrix, int size, string file_name)
-{
-    ifstream data;
-    data.open(file_name);
 
-    if (data.is_open())
-    {
-        for (int i = 0; i < matrix.size(); i++)
-        {
-            for (int j = 0; j < matrix.size(); j++)
-            {
-                data >> matrix[i][j];
-            }
+void read_matrix(vector<vector<int>>& matrix, string file_name) {
+    ifstream data(file_name);
+    for (auto& row : matrix) {
+        for (int& val : row) {
+            data >> val;
         }
     }
-    data.close();
-
 }
 
-void mul_matrix(const vector<vector<int>>& matrix_1, const vector<vector<int>>& matrix_2, vector<vector<int>>& matrix_result)
-{
-    for (int i = 0; i < matrix_result.size(); i++)
-    {
-        for (int j = 0; j < matrix_result[i].size(); j++)
-        {
+void mul_matrix(const vector<vector<int>>& matrix_1, const vector<vector<int>>& matrix_2, vector<vector<int>>& matrix_result) {
+    omp_set_num_threads(8);
+    int i, j, k;
+#pragma omp parallel for shared(matrix_1, matrix_2, matrix_result) private(i,j,k)
+    for (i = 0; i < matrix_result.size(); i++) {
+        for (j = 0; j < matrix_result[i].size(); j++) {
             matrix_result[i][j] = 0;
-            for (int k = 0; k < matrix_2.size(); k++)
-            {
+            for (k = 0; k < matrix_2.size(); k++) {
                 matrix_result[i][j] += matrix_1[i][k] * matrix_2[k][j];
             }
-
         }
     }
 }
-void write_result(string str, vector<vector<int>>& matrix_result, int size, clock_t end, clock_t start)
-{
+
+void write_result(string str, const vector<vector<int>>& matrix_result, double duration, string arr) {
     ofstream data(str);
-    ofstream result("result.txt", ios::app);
-
-
-    for (int i = 0; i < matrix_result.size(); i++)
-    {
-        for (int j = 0; j < matrix_result[i].size(); j++)
-        {
-            data << " " << matrix_result[i][j];
+    for (const auto& row : matrix_result) {
+        for (int val : row) {
+            data << " " << val;
         }
         data << endl;
     }
-
-    result << "Размер: " << size << " Время: " << (double(end - start)) / (double(CLOCKS_PER_SEC)) << endl;
-    data.close();
-    result.close();
+    ofstream result("result_8.txt", ios::app);
+    result << "Размер: " << matrix_result.size() << " Время: " << duration << " секунд" << endl << arr << endl;
 }
 
-int main()
-{
-    srand(time(NULL));
-    int SIZE = 10;
-    vector<vector<int>> matrix_1(SIZE, vector<int>(SIZE));
-    vector<vector<int>> matrix_2(SIZE, vector<int>(SIZE));
-    vector<vector<int>> matrix_result(SIZE, vector<int>(SIZE));
-    create_matrix("Matrix_1.txt", SIZE);
-    create_matrix("Matrix_2.txt", SIZE);
-    read_matrix(matrix_1, SIZE, "Matrix_1.txt");
-    read_matrix(matrix_2, SIZE, "Matrix_2.txt");
+int main() {
+    setlocale(LC_ALL, "Russian");
+    srand(static_cast<unsigned int>(time(nullptr)));
+    int SIZE[9] = { 100, 300, 500, 800, 1000, 1300, 1500, 1800, 2000};
 
-    clock_t start, end;
-    start = clock();
-    mul_matrix(matrix_1, matrix_2, matrix_result);
-    end = clock();
+    for (int i = 0; i < 9; i++)
+    {
+        vector<vector<int>> matrix_1(SIZE[i], vector<int>(SIZE[i]));
+        vector<vector<int>> matrix_2(SIZE[i], vector<int>(SIZE[i]));
+        vector<vector<int>> matrix_result(SIZE[i], vector<int>(SIZE[i]));
 
+        create_matrix("Matrix_1.txt", SIZE[i]);
+        create_matrix("Matrix_2.txt", SIZE[i]);
 
-    write_result("Matrix_res.txt", matrix_result, SIZE, end, start);
+        read_matrix(matrix_1, "Matrix_1.txt");
+        read_matrix(matrix_2, "Matrix_2.txt");
+        double result = 0;
+        string arr;
 
+        for (int i = 0; i < 10; i++)
+        {
+            auto start = chrono::high_resolution_clock::now();
+            mul_matrix(matrix_1, matrix_2, matrix_result);
+            auto end = chrono::high_resolution_clock::now();
+            double duration = chrono::duration_cast<chrono::milliseconds>(end - start).count() / 1000.0;
+            result += duration;
+            arr.append(to_string(duration) + "  ");
+        }
+
+        result /= 10;
+        cout << "Количество ядер: " << omp_get_max_threads() << endl;
+        write_result("Matrix_res.txt", matrix_result, result, arr);
+    }
 }
